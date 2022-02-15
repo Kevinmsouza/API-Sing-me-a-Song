f0affc46b50ea071d5201b4c4cd84690bde2d13f
Kevinmsouza <kevinm.souza@hotmail.com>
2022-02-15 19:47:30 -0400

    fix: database file for heroku deploy

--- src/database.js
+++ src/database.js
@@ -8,15 +8,26 @@ const {
     DB_HOST,
     DB_PORT,
     DB_DATABASE,
+    NODE_ENV,
+    DATABASE_URL,
 } = process.env;
 
-const databaseConfig = {
+let databaseConfig = {
     user: DB_USER,
     password: DB_PASSWORD,
     host: DB_HOST,
     port: DB_PORT,
     database: DB_DATABASE,
 };
+
+if (NODE_ENV === 'prod') {
+    databaseConfig = {
+        connectionString: DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    };
+}
 
 const connection = new Pool(databaseConfig);
 
